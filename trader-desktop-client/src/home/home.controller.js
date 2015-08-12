daaff3257e4ba3a26736d12b67586fb69f5593d9
varunnagarajan <varunnagarajan@ymail.com>
2015-08-12 22:46:01 +0530

    resume started

--- trader-desktop-client/src/home/home.controller.js
+++ trader-desktop-client/src/home/home.controller.js
@@ -1,4 +1,14 @@
 'use strict';
 angular.module('myApp.home')
-    .controller('HomeController',function(){
-});
+    .controller('HomeController',function($scope,$location){
+        $scope.gotoResume = function(){
+            $location.path('/resume');
+        };
+        $scope.gotoHobbies = function(){
+            $location.path('/hobbies');
+        };
+        $scope.gotoRecognitions = function(){
+            $location.path('/recognitions');
+        };
+        
+    });
