2d1f3562a0437eff944136cea61ad69802955c31
varunnagarajan <varunnagarajan@ymail.com>
2015-08-13 19:15:00 +0530

    minor styling fixes done

--- trader-desktop-client/src/contact/contact.controller.js
+++ trader-desktop-client/src/contact/contact.controller.js
@@ -1,21 +1,21 @@
 'use strict';
-/*globals*/
+/*globals google*/
 angular.module('myApp.contact')
     .controller('contactController',function(){
         function initialize() {
             var myLatlng = new google.maps.LatLng(12.9548195, 77.7171943);
             var marker = new google.maps.Marker({
                 position: myLatlng,
-                title:"My house!"
+                title:'My house!'
             });
             var mapCanvas = document.getElementById('map');
             var mapOptions = {
                 center: new google.maps.LatLng(12.9548195, 77.7171943),
                 zoom: 15,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
+            };
             var map = new google.maps.Map(mapCanvas, mapOptions);
             marker.setMap(map);
-        };
+        }
         initialize();
     });
